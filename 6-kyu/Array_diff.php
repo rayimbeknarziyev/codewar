e56f81711f7307af51e5b6e3edd24eb04d347f3e
Rayimbek <rayimbeknarziyev2@gmail.com>
2026-05-20 00:14:51 +0500

    task Array.diff completed

---
+++ 6-kyu/Array_diff.php
@@ -0,0 +1,12 @@
+<?php
+
+function arrayDiff($a, $b) {
+    $third = [];
+    
+    for($i = 0; $i < count($a); $i++){
+      if(!in_array($a[$i], $b)){
+        $third[] = $a[$i]; 
+      }
+    }
+    return $third;
+  }
