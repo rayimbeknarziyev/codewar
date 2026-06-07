b97b14814c11cd7b9177172986a0e1efa83b841c
Rayimbek <rayimbeknarziyev2@gmail.com>
2026-06-07 21:26:08 +0500

    Task Welcome! completed

---
+++ 8-kyu/Welcome!.php
@@ -0,0 +1,24 @@
+<?php
+function greet(string $language): string {
+   $languages = [
+        "english" => "Welcome",
+        "czech" => "Vitejte",
+        "danish" => "Velkomst",
+        "dutch" => "Welkom",
+        "estonian" => "Tere tulemast",
+        "finnish" => "Tervetuloa",
+        "flemish" => "Welgekomen",
+        "french" => "Bienvenue",
+        "german" => "Willkommen",
+        "irish" => "Failte",
+        "italian" => "Benvenuto",
+        "latvian" => "Gaidits",
+        "lithuanian" => "Laukiamas",
+        "polish" => "Witamy",
+        "spanish" => "Bienvenido",
+        "swedish" => "Valkommen",
+        "welsh" => "Croeso"
+    ];
+  
+  return $languages[$language] ?? "Welcome";
+}
